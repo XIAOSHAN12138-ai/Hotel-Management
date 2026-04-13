f8022d758c3bb23949f69e2ae699f15cc69c4661
xiaoshan12138 <1198547908@qq.com>
2026-04-13 21:54:26 +0800

    Initial commit

---
+++ HotelManagerUserVue/src/components/copyright.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Description:
+
+
+-->
+<template>
+  <el-row>
+    <el-col :span="24">
+      <!-- <p></p> -->
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {}
+    },
+    methods: {
+
+    },
+  }
+</script>
+
+<style scoped="scoped">
+  .el-row {
+    min-height: 6vh;
+    font-size: 1rem;
+  }
+
+  p {
+    margin: 0.5rem;
+  }
+</style>
